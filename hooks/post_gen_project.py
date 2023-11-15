ddb4598fd2c7487d906e614b35d06ff9eb78e834
Mathieu Marauri <mathieu.marauri@mfglabs.com>
2023-11-15 15:47:20 +0100

    Install poetry-up plugin, change Makefile to have a proper install command

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,7 @@
 import os
 
-os.system("make install")
+os.system("poetry self add poetry-plugin-up")
+os.system("poetry up --latest")
 os.system("git init")
 os.system("git add .")
 os.system("git commit -m 'Setup project'")
