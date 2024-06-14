9d4c38b5eb0eb60a7dc7533b18945aa3c6f9cec0
Mathieu Marauri <mathieu.marauri@mfglabs.com>
2024-06-14 17:21:26 +0200

    Use pyenv local + remove gitkeep files

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,9 +1,12 @@
 import os
 
+python_version = "{{cookiecutter.python_version}}"
+
+os.system("pyenv local " + python_version)
 os.system("poetry self add poetry-plugin-up")
 os.system("poetry up --latest")
 os.system("git init")
 os.system("git add .")
 os.system("git commit -m 'Setup project'")
 os.system("pre-commit install")
-# os.system("find . -type f -name '.gitkeep' -delete")
+os.system("find . -type f -name '.gitkeep' -delete")
