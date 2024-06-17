8e6ed2c08c70c3870f3f6a47a1775a3f69f9925c
Mathieu Marauri <mathieu.marauri@mfglabs.com>
2024-06-17 14:54:29 +0200

    Specify the python version to use in poetry env

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,7 +3,7 @@ import os
 python_version = "{{cookiecutter.python}}"
 
 os.system("pyenv local " + python_version)
-os.system("print ---------")
+os.system("poetry env use " + python_version)
 os.system("poetry self add poetry-plugin-up")
 os.system("poetry up --latest")
 os.system("git init")
