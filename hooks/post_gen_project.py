04eb0cb7455c1771fa928ba078575ce51954af4e
Mathieu Marauri <mathieu.marauri@mfglabs.com>
2024-06-17 13:56:28 +0200

    Test pyenv local

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,6 +3,7 @@ import os
 python_version = "{{cookiecutter.python}}"
 
 os.system("pyenv local " + python_version)
+os.system("print ---------")
 os.system("poetry self add poetry-plugin-up")
 os.system("poetry up --latest")
 os.system("git init")
