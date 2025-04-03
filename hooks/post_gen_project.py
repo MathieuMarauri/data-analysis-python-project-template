6702363b195e43e0f7da2b84f35426b69b59f746
Mathieu Marauri <mathieu.marauri@mfglabs.com>
2025-04-03 13:50:33 +0200

    Remove call to pyenv local

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,9 +1,7 @@
 import os
 
-python_version = "{{cookiecutter.python}}"
-
-os.system("pyenv local " + python_version)
-os.system("poetry env use " + python_version)
+# os.system("pyenv local " + "{{cookiecutter.python}}")
+os.system("poetry env use " + "{{cookiecutter.python}}")
 os.system("poetry self add poetry-plugin-up")
 os.system("poetry up --latest")
 os.system("git init")
